d1524ccb8cfe0cabdcb21149984ec5397e0a7641
Will Hilton <wmhilton@gmail.com>
2016-11-17 10:17:57 -0500

    Added sshpass. Now you can run 'sshpass -e scp ...' in scripts!

--- scripts/xampp/setup-ssh.sh
+++ scripts/xampp/setup-ssh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 set +x
-apt update && apt install openssh-client -y && apt clean
+apt update && apt install openssh-client sshpass -y && apt clean
 mkdir -p ~/.ssh
 echo 'StrictHostKeyChecking no' >> ~/.ssh/config
