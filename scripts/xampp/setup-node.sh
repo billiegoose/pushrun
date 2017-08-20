ae2fe31b75f0964b24e19c4d5e496a98350ca205
Will Hilton <wmhilton@gmail.com>
2017-08-20 19:19:43 -0400

    Update Node from 4.5 to 8.4 in xampp image

--- scripts/xampp/setup-node.sh
+++ scripts/xampp/setup-node.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -e
 set +x
-# net-tools is needed by the XAMPP start service script.
 # wget is only used to download the installer.
 # xz-utils is used by tar to extract Nodejs
 apt update && apt install wget xz-utils -y && apt clean
 # Install Nodejs
 cd /opt
-wget https://nodejs.org/dist/v4.5.0/node-v4.5.0-linux-x64.tar.xz
+wget https://nodejs.org/dist/v8.4.0/node-v8.4.0-linux-x64.tar.xz
 tar xf node-*.tar.xz
 rm node-*.tar.xz
 mv node-* node
@@ -15,6 +14,6 @@ mv node-* node
 source /etc/environment
 echo "PATH=\"/opt/node/bin:$PATH\"" >> /etc/environment
 source /etc/environment
-# Update npm2 to npm3
-npm install --global npm@3
+# Update npm
+npm install --global npm@latest
 npm list -g
