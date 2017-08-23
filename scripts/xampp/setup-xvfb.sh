42c6c1e92a3bdc55def21102c42e16883959aaae
William Hilton <wmhilton@gmail.com>
2017-08-23 15:03:07 -0400

    Try again

--- scripts/xampp/setup-xvfb.sh
+++ scripts/xampp/setup-xvfb.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 set +x
-apt update && apt install xvfb -y && apt clean
+apt update && apt install libgtk2.0-0 libgconf-2-4 libasound2 libxtst6 libxss1 libnss3 xvfb -y && apt clean
